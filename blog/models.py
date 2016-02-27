07983a5c3a5ba1d5445cf30668e568c5dee88d3b
Your Name <you@example.com>
2016-02-27 17:52:06 -0500

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -8,8 +8,10 @@ class Post(models.Model):
 	text = models.TextField()
 	created_date = models.DateTimeField(
 		blank=True, null=True)
+	published_date = models.DateTimeField(
+			blank=True, null=True)	
 
-	def publish(Self):
+	def publish(self):
 		self.published_date = timezone.now()
 		self.save()
 
